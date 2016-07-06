8ed03f9bc7b6a3f3b7745672b5cea823fb601de5
Ravindran Navaneethan <rnavaneethan@gmail.com>
2016-07-06 13:59:41 +0530

    Image processing is combined using stream-combiner2 to improve parallel processing.

--- gulpfile.js
+++ gulpfile.js
@@ -3,39 +3,49 @@ const gulp = require('gulp'),
   del = require('del'),
   imageminMozjpeg = require('imagemin-mozjpeg'),
   parallel = require('concurrent-transform'),
+  combiner = require('stream-combiner2'),
   os = require("os"),
   $ = require('gulp-load-plugins')(), //load all gulp plugins
   width = 1024, /*customizable config*/
   height = 1024,
   quality = 0.75,
   bCreateWebp = false,
-  SRC = './in/*.{jpg,jpeg,JPG,JPEG}',
+  SRC = './in/*.{jpg,jpeg,png,svg,gif}',
   DEST = './out/';
+
+/*image processing pipeline
+  - Resize the image.
+  - Minify the image with imagemin
+  - Mozjpeg compression is used for jpg files
+*/
+const processImages = combiner.obj(
+  $.imageResize({
+    'width': width,
+    'height': height,
+    'crop': false,
+    'autoOrient': true,
+    'overwrite': false,
+    'upscale': false,
+    'quality': quality
+  }),
+  $.imagemin({
+    progressive: true,
+    optimizationLevel: 7,
+    cache: false,
+    use: [imageminMozjpeg()]
+  })
+);
+
 gulp.task('clean', () => {
   return del(DEST + '/**/*'); //delete all destination files, just to be clean
 });
 gulp.task('default', () => {
   // place code here
-  return gulp.src(SRC)
+  return gulp.src(SRC, {nocase:true})
     .pipe($.plumber())  //node stream related error handling
     .pipe($.changed(DEST))
     .pipe($.sizediff.start())
-    .pipe(parallel($.imageResize({
-        'width': width,
-        'height': height,
-        'crop': false,
-        'autoOrient': true,
-        'overwrite': false,
-        'upscale': false,
-        'quality': quality
-      })
-    ), os.cpus().length)
-    .pipe(parallel($.imagemin({
-      progressive: true,
-      optimizationLevel: 7,
-      cache: false,
-      use: [imageminMozjpeg()]
-    })), os.cpus().length)
+    .pipe(parallel(processImages,os.cpus().length))
     .pipe(gulp.dest(DEST))
     .pipe(bCreateWebp ? $.webp() : $.util.noop())
     .pipe(gulp.dest(DEST))
